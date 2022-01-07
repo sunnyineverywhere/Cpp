c0d0efee0976fdad3f750dfba7276e4a0d48a38e
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2022-01-08 00:23:29 +0900

    별 찍기

---
+++ 2022.01/BOJ2741.cpp
@@ -0,0 +1,15 @@
+/* น้มุ 2741 N ย๏ฑโ */
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << i << "\n";
+	}
+
+	return 0;
+}
