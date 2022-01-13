a6198e7ab67d1e56923383ef963c0c79e15420de
SEON UI LEE <80109963+sunnyineverywhere@users.noreply.github.com>
2022-01-14 02:31:27 +0900

    기찍 N

---
+++ 2022.01/BOJ2742.cpp
@@ -0,0 +1,15 @@
+/* 백준 2742번 기찍 N */
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	for (int i = n; i >= 1; i--) {
+		cout << i << "\n";
+	}
+
+	return 0;
+}
