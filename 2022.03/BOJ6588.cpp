936587961f623b205dab4123a7386116a242b017
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2022-03-27 23:55:24 +0900

    [틀림] 골드바흐의 추측

--- 2022.03/BOJ6588.cpp
+++ 2022.03/BOJ6588.cpp
@@ -2,31 +2,36 @@
 #include <vector>
 using namespace std;
 
-vector<bool> pn(1000001, true);
+vector<int> pn(1000001, 0);
 
 void Eratos() {
-	pn[0] = false;
-	pn[1] = false;
-	pn[2] = false;
 
-	for (int i = 2; i*i <= 1000000; i++) {
-		if (pn[i]) {
-			for (int j = i * i; j <= 1000000; j += i) {
-				pn[j] = false;
-			}
-		}
-	}
 }
 
 int main(void) {
-	int n = 1;
-	while (n != 0) {
+
+	pn[0] = 1;
+	pn[1] = 1;
+	pn[2] = 1;
+
+	for (int i = 2; i * i <= 1000000; i++) {
+		if (pn[i] == 0) {
+			for (int j = i * i; j <= 1000000; j += i) {
+				pn[j] = 1;
+			}
+		}
+	}
+
+	int n;
+	while (1) {
 		cin >> n;
 		int left = 3;
 		int right = n - 3;
 		while (left <= right) {
-			if (pn[left] && pn[right]) {
-				break;
+			if (!pn[left] && !pn[right]) {
+				if (left + right == n) {
+					break;
+				}
 			}
 			left += 2;
 			right -= 2;
