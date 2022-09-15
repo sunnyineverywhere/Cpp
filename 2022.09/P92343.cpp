fe240737815108c8c7160ba60c1265ecda2ba68e
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2022-09-16 01:08:20 +0900

    [미완] 양과 늑대

---
+++ 2022.09/P92343.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0; // 양
+int wolves = 0;
+bool visited[18];
+
+
+int solution(vector<int> info, vector<vector<int>> edges) {
+    
+    return answer;
+}
