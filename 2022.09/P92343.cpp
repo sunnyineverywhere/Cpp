ac35898480808f0d0b3a0b73ad43e517e49c8265
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2022-09-17 00:26:51 +0900

    Update P92343.cpp

--- 2022.09/P92343.cpp
+++ 2022.09/P92343.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
+int n;
 int answer = 0; // 양
 int wolves = 0;
 bool visited[18];
 
 
 int solution(vector<int> info, vector<vector<int>> edges) {
+    int n = info.size();
     
     return answer;
 }
