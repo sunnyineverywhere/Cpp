385d4c76a9d45fdd345993407f460894128ba16a
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2022-09-20 01:31:50 +0900

    [실패] 기둥과 보 설치

---
+++ 2022.09/P60061.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+// 보 설치 - 한쪽 끝이 기둥 위, 양쪽 끝 부분이 다른 보와 연결
+
+// 기둥 설치 - 바닥 위, 보의 한쪽 끝 부분 위, 다른 기둥 위
+
+// x, y, a, b
+// x, y: 기둥/보를 설치 또는 삭제할 교차점의 좌표
+// a는 종류: 0은 기둥, 1은 보
+// b는 구조물 설치 여부: 0은 삭제, 1은 설치
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    vector<vector<int>> answer;
+    
+    
+    
+    return answer;
+}
